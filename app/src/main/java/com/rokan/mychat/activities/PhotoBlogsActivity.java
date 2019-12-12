f3845436f9f0c1a5896f2bcdecabd08586e4ed66
Rokan Uddin <ibrar.tanim@gmail.com>
2019-12-12 16:27:12 +0600

    minor

--- app/src/main/java/com/rokan/mychat/activities/PhotoBlogsActivity.java
+++ app/src/main/java/com/rokan/mychat/activities/PhotoBlogsActivity.java
@@ -4,10 +4,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.rokan.mychat.R;
+import com.rokan.mychat.adapters.PhotoBlogsAdapter;
+import com.rokan.mychat.pojo.LiveChat;
+import com.rokan.mychat.pojo.PhotoBlogs;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class PhotoBlogsActivity extends AppCompatActivity {
 
     PhotoBlogsActivity activity;
+    private PhotoBlogsAdapter photoBlogsAdapter;
+    private List<PhotoBlogs> photoBlogsList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,7 +23,41 @@ public class PhotoBlogsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_photo_blogs);
 
         activity = this;
+        photoBlogsList = new ArrayList<>();
+        photoBlogsAdapter = new PhotoBlogsAdapter(activity, photoBlogsList);
+        preparePhotoBlogs();
     }
 
+    private void preparePhotoBlogs() {
+
+        PhotoBlogs photoBlogs = new PhotoBlogs("15", "3", "9", "Mr.Ahmed", "Ibrar Tanim Rokan");
+        photoBlogsList.add(photoBlogs);
+
+        photoBlogs = new PhotoBlogs("15", "3", "9", "Mr.Ahmed", "Ibrar Tanim Rokan");
+        photoBlogsList.add(photoBlogs);
+
+        photoBlogs = new PhotoBlogs("15", "3", "9", "Mr.Ahmed", "Ibrar Tanim Rokan");
+        photoBlogsList.add(photoBlogs);
+
+        photoBlogs = new PhotoBlogs("15", "3", "9", "Mr.Ahmed", "Ibrar Tanim Rokan");
+        photoBlogsList.add(photoBlogs);
+
+        photoBlogs = new PhotoBlogs("15", "3", "9", "Mr.Ahmed", "Ibrar Tanim Rokan");
+        photoBlogsList.add(photoBlogs);
+
+        photoBlogs = new PhotoBlogs("15", "3", "9", "Mr.Ahmed", "Ibrar Tanim Rokan");
+        photoBlogsList.add(photoBlogs);
+
+        photoBlogs = new PhotoBlogs("15", "3", "9", "Mr.Ahmed", "Ibrar Tanim Rokan");
+        photoBlogsList.add(photoBlogs);
+
+        photoBlogs = new PhotoBlogs("15", "3", "9", "Mr.Ahmed", "Ibrar Tanim Rokan");
+        photoBlogsList.add(photoBlogs);
+
+        photoBlogs = new PhotoBlogs("15", "3", "9", "Mr.Ahmed", "Ibrar Tanim Rokan");
+        photoBlogsList.add(photoBlogs);
+
+        photoBlogsAdapter.notifyDataSetChanged();
+    }
 
 }
