a9a3ff13f5185048ac2821f62a0cb259453f4298
Rokan Uddin <ibrar.tanim@gmail.com>
2019-12-19 11:40:46 +0600

    Photoblogs updated

--- app/src/main/java/com/rokan/mychat/activities/PhotoBlogsActivity.java
+++ app/src/main/java/com/rokan/mychat/activities/PhotoBlogsActivity.java
@@ -8,6 +8,7 @@ import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.util.TypedValue;
 import android.view.View;
 
@@ -51,15 +52,21 @@ public class PhotoBlogsActivity extends AppCompatActivity {
 
         hotLists = new ArrayList<>();
         hotListAdapter = new HotListAdapter(activity, hotLists);
-        layoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
 
+        layoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
         rvHotList.setLayoutManager(layoutManager);
         rvHotList.setAdapter(hotListAdapter);
 
-        RecyclerView.LayoutManager mLayoutManager = new GridLayoutManager(activity, 2);
+        /*RecyclerView.LayoutManager mLayoutManager = new GridLayoutManager(activity, 2);
         rvPhotoBlogs.setLayoutManager(mLayoutManager);
         rvPhotoBlogs.addItemDecoration(new GridSpacingItemDecoration(2, GridSpacingItemDecoration.dpToPx(10, activity), true));
+        rvPhotoBlogs.setItemAnimator(new DefaultItemAnimator());*/
+
+        StaggeredGridLayoutManager staggeredGridLayoutManager = new StaggeredGridLayoutManager(2, LinearLayoutManager.VERTICAL);
+        rvPhotoBlogs.setLayoutManager(staggeredGridLayoutManager);
+        rvPhotoBlogs.addItemDecoration(new GridSpacingItemDecoration(2, GridSpacingItemDecoration.dpToPx(10, activity), true));
         rvPhotoBlogs.setItemAnimator(new DefaultItemAnimator());
+
         rvPhotoBlogs.setAdapter(photoBlogsAdapter);
 
 
@@ -124,28 +131,36 @@ public class PhotoBlogsActivity extends AppCompatActivity {
                 R.mipmap.samsurrahman,
         };
 
-        PhotoBlogs photoBlogs = new PhotoBlogs(listItemPhotoBlogs[0], listItemPhotoBlogs[0], "13", "3", "9", "Kazi Nazrul Islam our national poet, We Love him from our heart. Kazi Nazrul Islam our national poet, We Love him from our heart");
+        PhotoBlogs photoBlogs = new PhotoBlogs(listItemPhotoBlogs[0], listItemPhotoBlogs[0], "13", "3", "9",
+                "ঢাকার সাভারের উত্তর ও পশ্চিম কাউন্দিয়া এলাকায় গতকাল বুধবার অভিযান চালিয়ে চারটি ইটভাটা বন্ধ করে দিয়েছে পরিবেশ অধিদপ্তরের ভ্রাম্যমাণ আদালত।");
         photoBlogsList.add(photoBlogs);
 
-        photoBlogs = new PhotoBlogs(listItemPhotoBlogs[1], listItemPhotoBlogs[1], "13", "3", "9", getString(R.string.jafariqbal));
+        photoBlogs = new PhotoBlogs(listItemPhotoBlogs[1], listItemPhotoBlogs[1], "13", "3", "9",
+                "ছাড়পত্র ছাড়া ইট পোড়ানোর অভিযোগে এসব ভাটাকে ৩৩ লাখ টাকা জরিমানা করা হয়েছে।");
         photoBlogsList.add(photoBlogs);
 
-        photoBlogs = new PhotoBlogs(listItemPhotoBlogs[2], listItemPhotoBlogs[2], "13", "3", "9",  getString(R.string.sufiakamal));
+        photoBlogs = new PhotoBlogs(listItemPhotoBlogs[2], listItemPhotoBlogs[2], "13", "3", "9",
+                "আংশিক ভেঙে দেওয়া হয়েছে তিনটি ভাটা।");
         photoBlogsList.add(photoBlogs);
 
-        photoBlogs = new PhotoBlogs(listItemPhotoBlogs[3], listItemPhotoBlogs[3], "13", "3", "9", "Kazi Nazrul Islam our national poet, We Love him from our heart. Kazi Nazrul Islam our national poet, We Love him from our heart");
+        photoBlogs = new PhotoBlogs(listItemPhotoBlogs[3], listItemPhotoBlogs[3], "13", "3", "9",
+                "জরিমানার টাকা পরিশোধ করতে না পারায় আটক করা হয়েছে দুজনকে। ");
         photoBlogsList.add(photoBlogs);
 
-        photoBlogs = new PhotoBlogs(listItemPhotoBlogs[4], listItemPhotoBlogs[4], "13", "3", "9", "Kazi Nazrul Islam our national poet, We Love him from our heart. Kazi Nazrul Islam our national poet, We Love him from our heart");
+        photoBlogs = new PhotoBlogs(listItemPhotoBlogs[4], listItemPhotoBlogs[4], "13", "3", "9",
+                "অন্যদিকে নারায়ণগঞ্জের রূপগঞ্জে অবৈধ ছয়টি ইটভাটা উচ্ছেদ করেছেন ভ্রাম্যমাণ আদালত।");
         photoBlogsList.add(photoBlogs);
 
-        photoBlogs = new PhotoBlogs(listItemPhotoBlogs[5], listItemPhotoBlogs[5], "13", "3", "9", getString(R.string.robindronath));
+        photoBlogs = new PhotoBlogs(listItemPhotoBlogs[5], listItemPhotoBlogs[5], "13", "3", "9",
+                "একই সঙ্গে এসব ইটভাটাকে ১৫ লাখ টাকা জরিমানা করা হয়েছে।");
         photoBlogsList.add(photoBlogs);
 
-        photoBlogs = new PhotoBlogs(listItemPhotoBlogs[6], listItemPhotoBlogs[6], "13", "3", "9", getString(R.string.humayanahmed));
+        photoBlogs = new PhotoBlogs(listItemPhotoBlogs[6], listItemPhotoBlogs[6], "13", "3", "9",
+                "ফায়ার সার্ভিসের গাড়ির পাম্পের সাহায্যে কাঁচা ইট বিনষ্ট ও ইট পোড়ানো কার্যক্রম বন্ধ করে দেওয়া হয়।");
         photoBlogsList.add(photoBlogs);
 
-        photoBlogs = new PhotoBlogs(listItemPhotoBlogs[7], listItemPhotoBlogs[7], "13", "3", "9", "Kazi Nazrul Islam our national poet, We Love him from our heart. Kazi Nazrul Islam our national poet, We Love him from our heart");
+        photoBlogs = new PhotoBlogs(listItemPhotoBlogs[7], listItemPhotoBlogs[7], "13", "3", "9",
+                "সাভারে পরিবেশ অধিদপ্তরের নির্বাহী ম্যাজিস্ট্রেট মাকছুদুল ইসলাম অভিযান চালান।");
         photoBlogsList.add(photoBlogs);
 
 
