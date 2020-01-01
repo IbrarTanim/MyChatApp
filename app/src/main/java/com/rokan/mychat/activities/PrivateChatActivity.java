6504f6d924e1a0e42e6a35d8c5246485de8ab8ae
Rokan Uddin <ibrar.tanim@gmail.com>
2020-01-01 16:26:16 +0600

    dgrg

--- app/src/main/java/com/rokan/mychat/activities/PrivateChatActivity.java
+++ app/src/main/java/com/rokan/mychat/activities/PrivateChatActivity.java
@@ -3,18 +3,91 @@ package com.rokan.mychat.activities;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
 
 import com.rokan.mychat.R;
+import com.rokan.mychat.adapters.PrivateChatAdapter;
+import com.rokan.mychat.adapters.UserPhotoBlogsAdapter;
+import com.rokan.mychat.pojo.UserPhotoBlogs;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class PrivateChatActivity extends AppCompatActivity {
     PrivateChatActivity activity;
+
+    private PrivateChatAdapter  privateChatAdapter;
+    private List<UserPhotoBlogs> privateChatList;
+    private RecyclerView rvPrivateChat;
+    LinearLayoutManager layoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_private_chat);
         activity = this;
+
+
+        rvPrivateChat = findViewById(R.id.rvUserPhotoBlogs);
+        privateChatList = new ArrayList<>();
+
+
+        privateChatAdapter = new PrivateChatAdapter(activity, privateChatList);
+        StaggeredGridLayoutManager staggeredGridLayoutManager = new StaggeredGridLayoutManager(2, LinearLayoutManager.VERTICAL);
+        rvPrivateChat.setLayoutManager(staggeredGridLayoutManager);
+        rvPrivateChat.setAdapter(privateChatAdapter);
+
+
+
+        preparePhotoBlogs();
     }
+
+    private void preparePhotoBlogs() {
+
+
+        int[] listItemPhotoBlogs = new int[]{
+                R.mipmap.chatting,
+                R.mipmap.humayunahmed,
+                R.mipmap.find_your_love,
+                R.mipmap.anisul,
+                R.mipmap.find_friends,
+                R.mipmap.jaforiqbal,
+                R.mipmap.taking_photo,
+                R.mipmap.samsurrahman,
+        };
+
+        UserPhotoBlogs userPhotoBlogs = new UserPhotoBlogs(listItemPhotoBlogs[0]);
+        privateChatList.add(userPhotoBlogs);
+
+        userPhotoBlogs = new UserPhotoBlogs(listItemPhotoBlogs[1]);
+        privateChatList.add(userPhotoBlogs);
+
+        userPhotoBlogs = new UserPhotoBlogs(listItemPhotoBlogs[2]);
+        privateChatList.add(userPhotoBlogs);
+
+        userPhotoBlogs = new UserPhotoBlogs(listItemPhotoBlogs[3]);
+        privateChatList.add(userPhotoBlogs);
+
+        userPhotoBlogs = new UserPhotoBlogs(listItemPhotoBlogs[4]);
+        privateChatList.add(userPhotoBlogs);
+
+        userPhotoBlogs = new UserPhotoBlogs(listItemPhotoBlogs[5]);
+        privateChatList.add(userPhotoBlogs);
+
+        userPhotoBlogs = new UserPhotoBlogs(listItemPhotoBlogs[6]);
+        privateChatList.add(userPhotoBlogs);
+
+        userPhotoBlogs = new UserPhotoBlogs(listItemPhotoBlogs[7]);
+        privateChatList.add(userPhotoBlogs);
+
+
+        privateChatAdapter.notifyDataSetChanged();
+    }
+
+
+
 
     @Override
     public void onBackPressed() {
---
+++ app/src/main/java/com/rokan/mychat/adapters/PrivateChatAdapter.java
@@ -0,0 +1,67 @@
+package com.rokan.mychat.adapters;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.makeramen.roundedimageview.RoundedImageView;
+import com.rokan.mychat.R;
+import com.rokan.mychat.activities.ViewImageActivity;
+import com.rokan.mychat.pojo.LiveChat;
+import com.rokan.mychat.pojo.PhotoBlogs;
+import com.rokan.mychat.pojo.UserPhotoBlogs;
+
+import java.util.List;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class PrivateChatAdapter extends RecyclerView.Adapter<PrivateChatAdapter.MyViewHolder> {
+
+
+    private Context mContext;
+    private List<UserPhotoBlogs> userPhotoBlogsList;
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        RoundedImageView rivPrivateChatImage;
+
+        public MyViewHolder(View view) {
+            super(view);
+            rivPrivateChatImage = view.findViewById(R.id.rivPrivateChatImage);
+
+        }
+
+    }
+
+
+    public PrivateChatAdapter(Context mContext, List<UserPhotoBlogs> userPhotoBlogsList) {
+        this.mContext = mContext;
+        this.userPhotoBlogsList = userPhotoBlogsList;
+    }
+
+    @Override
+    public PrivateChatAdapter.MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        final View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.private_chat_cell, parent, false);
+        final PrivateChatAdapter.MyViewHolder myViewHolder = new PrivateChatAdapter.MyViewHolder(itemView);
+
+        return myViewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(final PrivateChatAdapter.MyViewHolder holder, int position) {
+        UserPhotoBlogs userPhotoBlogs = userPhotoBlogsList.get(position);
+        Glide.with(mContext).load(userPhotoBlogs.getRoundedUserBlogProfilePic()).into(holder.rivPrivateChatImage);
+    }
+
+    @Override
+    public int getItemCount() {
+        return userPhotoBlogsList.size();
+    }
+}
