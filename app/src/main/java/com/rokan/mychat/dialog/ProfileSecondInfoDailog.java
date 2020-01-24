691a5b787cb582d3df0f1f3b5a843f5635dc5ac1
rokan <rokan@dohatec.com.bd>
2020-01-24 17:16:13 +0600

    hgjhmghj

--- app/src/main/java/com/rokan/mychat/dialog/ProfileSecondInfoDailog.java
+++ app/src/main/java/com/rokan/mychat/dialog/ProfileSecondInfoDailog.java
@@ -17,7 +17,7 @@ import com.rokan.mychat.activities.ProfileSecondActivity;
 public class ProfileSecondInfoDailog extends Dialog implements View.OnClickListener {
     ProfileSecondActivity activity;
 
-    LinearLayout llAbout;
+    LinearLayout llAbout, llRelationship, llLocationFor, llProfession, llEducation, llLanguage, llHeight, llHairColor, llEyeColor, llSmoking, llHobbies;
 
     TextView tvRlationship, tvLookingfor, tvProfession, tvEducation, tvLanguage;
     TextView tvHeight, tvHairColor, tvEyeColor, tvSmoking, tvHobbie;
@@ -26,7 +26,6 @@ public class ProfileSecondInfoDailog extends Dialog implements View.OnClickListe
 
     public ProfileSecondInfoDailog(ProfileSecondActivity activity) {
         super(activity, R.style.CustomAlertDialog);
-        //this.context = context;
         this.activity = activity;
     }
 
@@ -36,6 +35,18 @@ public class ProfileSecondInfoDailog extends Dialog implements View.OnClickListe
         setContentView(R.layout.info_dialog_profile_second);
 
         llAbout = findViewById(R.id.llAbout);
+        llRelationship = findViewById(R.id.llRelationship);
+        llLocationFor = findViewById(R.id.llLocationFor);
+        llProfession = findViewById(R.id.llProfession);
+        llEducation = findViewById(R.id.llEducation);
+        llLanguage = findViewById(R.id.llLanguage);
+        llHeight = findViewById(R.id.llHeight);
+        llHairColor = findViewById(R.id.llHairColor);
+        llEyeColor = findViewById(R.id.llEyeColor);
+        llSmoking = findViewById(R.id.llSmoking);
+        llHobbies = findViewById(R.id.llHobbies);
+
+
         tvRlationship = findViewById(R.id.tvRlationship);
         tvLookingfor = findViewById(R.id.tvLookingfor);
         tvProfession = findViewById(R.id.tvProfession);
@@ -49,16 +60,16 @@ public class ProfileSecondInfoDailog extends Dialog implements View.OnClickListe
         rivInfoProfileSecond = findViewById(R.id.rivInfoProfileSecond);
 
         llAbout.setOnClickListener(this);
-        tvRlationship.setOnClickListener(this);
-        tvLookingfor.setOnClickListener(this);
-        tvProfession.setOnClickListener(this);
-        tvEducation.setOnClickListener(this);
-        tvLanguage.setOnClickListener(this);
-        tvHeight.setOnClickListener(this);
-        tvHairColor.setOnClickListener(this);
-        tvEyeColor.setOnClickListener(this);
-        tvSmoking.setOnClickListener(this);
-        tvHobbie.setOnClickListener(this);
+        llRelationship.setOnClickListener(this);
+        llLocationFor.setOnClickListener(this);
+        llProfession.setOnClickListener(this);
+        llEducation.setOnClickListener(this);
+        llLanguage.setOnClickListener(this);
+        llHeight.setOnClickListener(this);
+        llHairColor.setOnClickListener(this);
+        llEyeColor.setOnClickListener(this);
+        llSmoking.setOnClickListener(this);
+        llHobbies.setOnClickListener(this);
         rivInfoProfileSecond.setOnClickListener(this);
 
     }
@@ -74,53 +85,53 @@ public class ProfileSecondInfoDailog extends Dialog implements View.OnClickListe
                 DialogControl.ShowDialog(activity, aboutDialog);
                 break;
 
-            case R.id.tvRlationship:
+            case R.id.llRelationship:
                 RelationShipDialog relationShipDialog = new RelationShipDialog(activity);
                 DialogControl.ShowDialog(activity, relationShipDialog);
                 break;
 
 
-            case R.id.tvLookingfor:
+            case R.id.llLocationFor:
                 LookingforDialog lookingforDialog = new LookingforDialog(activity);
                 DialogControl.ShowDialog(activity, lookingforDialog);
                 break;
 
-            case R.id.tvProfession:
+            case R.id.llProfession:
                 ProfessionDialog professionDialog = new ProfessionDialog(activity);
                 DialogControl.ShowDialog(activity, professionDialog);
                 break;
 
-            case R.id.tvEducation:
+            case R.id.llEducation:
                 EducationDialog educationDialog = new EducationDialog(activity);
                 DialogControl.ShowDialog(activity, educationDialog);
                 break;
 
-            case R.id.tvLanguage:
+            case R.id.llLanguage:
                 LanguageDialog languageDialog = new LanguageDialog(activity);
                 DialogControl.ShowDialog(activity, languageDialog);
                 break;
 
-            case R.id.tvHeight:
+            case R.id.llHeight:
                 HeightDialog heightDialog = new HeightDialog(activity);
                 DialogControl.ShowDialog(activity, heightDialog);
                 break;
 
-            case R.id.tvHairColor:
+            case R.id.llHairColor:
                 HairColorDialog hairColorDialog = new HairColorDialog(activity);
                 DialogControl.ShowDialog(activity, hairColorDialog);
                 break;
 
-            case R.id.tvEyeColor:
+            case R.id.llEyeColor:
                 EyeColorDialog eyeColorDialog = new EyeColorDialog(activity);
                 DialogControl.ShowDialog(activity, eyeColorDialog);
                 break;
 
-            case R.id.tvSmoking:
+            case R.id.llSmoking:
                 SmokingDialog smokingDialog = new SmokingDialog(activity);
                 DialogControl.ShowDialog(activity, smokingDialog);
                 break;
 
-            case R.id.tvHobbie:
+            case R.id.llHobbies:
                 HobbiesDialog hobbiesDialog = new HobbiesDialog(activity);
                 DialogControl.ShowDialog(activity, hobbiesDialog);
                 break;
@@ -128,7 +139,6 @@ public class ProfileSecondInfoDailog extends Dialog implements View.OnClickListe
             case R.id.rivInfoProfileSecond:
                 activity.startActivity(new Intent(activity, ProfilePictureActivity.class));
                 break;
-
 
         }
     }
